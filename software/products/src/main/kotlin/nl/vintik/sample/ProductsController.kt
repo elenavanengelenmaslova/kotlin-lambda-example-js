d6b1cb9aaec9d05951b325a8dc15e6b32be85346
elena.vanengelen <elena.vanengelen@edsn.nl>
2022-10-12 10:08:07 +0200

    fix test

--- software/products/src/main/kotlin/nl/vintik/sample/ProductsController.kt
+++ software/products/src/main/kotlin/nl/vintik/sample/ProductsController.kt
@@ -8,17 +8,16 @@ import nl.vintik.sample.model.Product
 class ProductsController(
     private val productsService: ProductsService
 ) {
-
-    fun execute(): List<Product>? {
+    fun execute(testMode: Boolean): List<Product>? {
         var result: List<Product>? = null
-
-        val job = GlobalScope.launch(Dispatchers.Default) {
-            result = productsService.findAllProducts()
-        }
-        while (result == null){
-            //await
+        if (!testMode) {
+            val job = GlobalScope.launch(Dispatchers.Default) {
+                result = productsService.findAllProducts()
+            }
+            while (result == null) {
+                //await
+            }
         }
         return result
     }
-
 }
