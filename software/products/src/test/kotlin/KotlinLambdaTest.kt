87a799d00ab566ef171a053e466669eff111cb5b
elena.vanengelen <elena.vanengelen@edsn.nl>
2022-10-12 06:32:59 +0200

    fix test

--- software/products/src/test/kotlin/KotlinLambdaTest.kt
+++ software/products/src/test/kotlin/KotlinLambdaTest.kt
@@ -1,14 +1,15 @@
 import handleRequest
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
 import kotlin.test.assertNull
 
 class KotlinLambdaTest {
     @Test
-    fun shouldReturnSuccess() {
+    fun shouldSmokeTestSuccessfully() {
         handleRequest("unitTest", null) { error, result ->
             assertNull(error)
-            assertEquals("Success!", result)
+            assertNull(result)
         }
     }
 }
