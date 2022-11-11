0d10062da669587d5b8648c2a655fec685dbaa37
elena.vanengelen <elena.vanengelen@edsn.nl>
2022-11-11 14:35:50 +0100

    simplify code and fix pagination

--- software/products/src/main/kotlin/nl/vintik/sample/ProductsService.kt
+++ software/products/src/main/kotlin/nl/vintik/sample/ProductsService.kt
@@ -1,9 +1,12 @@
 package nl.vintik.sample
 
+import externals.client_dynamodb.AttributeValue
 import externals.client_dynamodb.DynamoDB
 import externals.client_dynamodb.ScanCommandInput
 import externals.client_dynamodb.ScanCommandOutput
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.await
+import kotlinx.coroutines.launch
 import nl.vintik.sample.model.Product
 import nl.vintik.sample.model.Product.Companion.TABLE_NAME
 import kotlin.js.Promise
@@ -11,36 +14,21 @@ import kotlin.js.Promise
 class ProductsService(private val dynamoDbClient: DynamoDB) {
     suspend fun findAllProducts(): List<Product> {
         val products = mutableSetOf<Product>()
-        val jobs = mutableListOf<Promise<ScanCommandOutput>>()
 
         console.log("Parallel scans set to : $parallelScanTotalSegments with page size $parallelScanPageSize")
 
-        for (segment in 0 until parallelScanTotalSegments) {
-            val input = object : ScanCommandInput {
-                // omitted
-                override var TableName: String?
-                    get() = TABLE_NAME
-                    set(value) {}
-            }
-            input.Segment = segment
-            input.TotalSegments = parallelScanTotalSegments
-            input.Limit = parallelScanPageSize
-            scan(jobs, input, products)
-
-        }
-        Promise.all(jobs.toTypedArray()).await()
+        Promise.all((0 until parallelScanTotalSegments).map { segment ->
+            scan(createScanCommandInput(segment), products)
+        }.toTypedArray()).await()
 
         console.log("number of Product: ${products.size}")
         return products.toList()
     }
 
     private fun scan(
-        jobs: MutableList<Promise<ScanCommandOutput>>,
-        input: ScanCommandInput,
-        products: MutableSet<Product>
-    ) {
+        input: ScanCommandInput, products: MutableSet<Product>
+    ): Promise<ScanCommandOutput> {
         val result = dynamoDbClient.scan(input)
-        jobs.add(result)
         result.then { scanOutput ->
             scanOutput.Items?.forEach { productData ->
                 products.add(
@@ -50,27 +38,30 @@ class ProductsService(private val dynamoDbClient: DynamoDB) {
                         (productData["price"].N as String).toFloat()
                     )
                 )
-
             }
             scanOutput.LastEvaluatedKey?.let {
                 if (input.ExclusiveStartKey != it) {
-                    val newInput = object : ScanCommandInput {
-                        // omitted
-                        override var TableName: String?
-                            get() = TABLE_NAME
-                            set(value) {}
-                    }
-                    newInput.Segment = input.Segment
-                    newInput.TotalSegments = input.TotalSegments
-                    newInput.Limit = input.Limit
-                    newInput.ExclusiveStartKey = it
-                    scan(jobs, newInput, products)
+                    GlobalScope.launch { scan(createScanCommandInput(input.Segment, it), products).await() }
                 }
             }
-
         }.catch {
             console.log("Error: ${JSON.stringify(it)}")
         }
+        return result
+    }
+
+    private fun createScanCommandInput(segment: Number?, exclusiveStartKey: AttributeValue? = null): ScanCommandInput {
+        val input = object : ScanCommandInput {
+            // omitted
+            override var TableName: String?
+                get() = TABLE_NAME
+                set(value) {}
+        }
+        input.Segment = segment
+        input.TotalSegments = parallelScanTotalSegments
+        input.Limit = parallelScanPageSize
+        input.ExclusiveStartKey = exclusiveStartKey
+        return input
     }
 
     companion object {
