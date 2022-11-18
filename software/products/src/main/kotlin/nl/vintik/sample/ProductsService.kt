4bc1b6d9b9971b98f453933718124bd89047a5b3
elena.vanengelen <elena.vanengelen@edsn.nl>
2022-11-18 12:05:11 +0100

    await pagination coroutines

--- software/products/src/main/kotlin/nl/vintik/sample/ProductsService.kt
+++ software/products/src/main/kotlin/nl/vintik/sample/ProductsService.kt
@@ -3,31 +3,30 @@ package nl.vintik.sample
 import externals.client_dynamodb.AttributeValue
 import externals.client_dynamodb.DynamoDB
 import externals.client_dynamodb.ScanCommandInput
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.await
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import nl.vintik.sample.model.Product
 import nl.vintik.sample.model.Product.Companion.TABLE_NAME
+import kotlin.js.Promise
 
 class ProductsService(private val dynamoDbClient: DynamoDB) {
     suspend fun findAllProducts(): List<Product> {
         val products = mutableListOf<Product>()
+        val jobs = mutableListOf<Job>()
 
         console.log("Parallel scans set to : $parallelScanTotalSegments with page size $parallelScanPageSize")
-
-        (0 until parallelScanTotalSegments).map { segment ->
-            scan(createScanCommandInput(segment), products)
-        }
+        Promise.all((0 until parallelScanTotalSegments).map { segment ->
+            scan(createScanCommandInput(segment), products, jobs)
+        }.toTypedArray()).await()
+        jobs.joinAll()
 
         console.log("total number of Product: ${products.size}")
         return products.toList()
     }
 
     private suspend fun scan(
-        input: ScanCommandInput, products: MutableList<Product>
-    ) {
-        dynamoDbClient.scan(input).await().let { scanOutput ->
+        input: ScanCommandInput, products: MutableList<Product>, jobs: MutableList<Job>
+    ): Promise<Unit> {
+        return dynamoDbClient.scan(input).then { scanOutput ->
             scanOutput.Items?.map { productData ->
                 products.add(
                     Product(
@@ -39,16 +38,18 @@ class ProductsService(private val dynamoDbClient: DynamoDB) {
             }
             scanOutput.LastEvaluatedKey?.let {
                 if (input.ExclusiveStartKey != it) {
-                    GlobalScope.launch(Dispatchers.Default) {
+                    jobs.add(GlobalScope.launch(Dispatchers.Default) {
                         scan(
                             createScanCommandInput(input.Segment, it),
-                            products
-                        )
-                    }
+                            products,
+                            jobs
+                        ).await()
+                    })
                 }
             }
+            console.log("scan products total: ${products.size} for input: ${JSON.stringify(input)}")
         }
-        console.log("scan products total: ${products.size} for input: ${JSON.stringify(input)}")
+
     }
 
     private fun createScanCommandInput(segment: Number?, exclusiveStartKey: AttributeValue? = null): ScanCommandInput {
